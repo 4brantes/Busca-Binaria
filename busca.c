7d9b9e7170455a336ce0c89ac64b31df952e7c9d
Guilherme Abrantes <guilherme.jkl.rn28@gmail.com>
2024-08-21 03:50:42 -0300

    ..

--- busca.c
+++ busca.c
@@ -24,6 +24,7 @@ int busca_binaria(int arr[], int tamanho, int elemento) {
     return -1; // Elemento não encontrado, retorna -1
 }
 int main() {
+    // Inicializar o array
     int arr[] = {2, 3, 4, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400};
     int tamanho = sizeof(arr) / sizeof(arr[0]);
 
